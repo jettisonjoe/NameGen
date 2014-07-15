08cfca899f47867ff6ca7f6f558d9ae3609d6e32
Joe <joeethier@gmail.com>
2014-07-14 18:31:46 -0700

    cleans up code a little

--- namegen.py
+++ namegen.py
@@ -3,38 +3,52 @@
 
 import random
 
+PARTS_OF_SPEECH = [ 'v',
+					'n',
+					'adj',
+					'adv' ]
+
 f = open("oed.txt")
 WORDLIST = f.readlines()
 f.close()
 
-def n_random_words(n):
-	result = []
-	while len(result) < n:
-		# The top (0th) line of oed.txt is purposely skipped.
+def random_entry():
+	result = ''
+	while result is '':
 		k = random.randint(1, len(WORDLIST))
 		line = WORDLIST[k]
 		if len(line) > 3:
-			result.append(WORDLIST[k])
+			result = line
 	return result
 
-
-def random_vlamb_style():
+def n_random_words(n):
 	result = []
-	while len(result) < 1:
-		line = n_random_words(1)[0]
-		if ' adj. ' in line: result.append(line)
-	while len(result) < 2:
-		line = n_random_words(1)[0]
-		if ' n. ' in line: result.append(line)
+	while len(result) < n:
+			result.append(random_entry())
 	return result
 
+def random_of_type(s):
+	assert s in PARTS_OF_SPEECH
+	result = ''
+	while result is '':
+		entry = random_entry()
+		if (s+'.') in entry:
+			result = entry
+	return result
 
+def random_v_style():
+	result = []
+	result.append(random_of_type('adj'))
+	result.append(random_of_type('n'))
+	return result
 
 def main():
 	random.seed()
 
 	#print '\n'.join(n_random_words(2))
-	print '\n'.join(random_vlamb_style())
+	#print '\n'.join(random_vlamb_style())
+	#print random_entry()
+	print '\n'.join(random_v_style())
 
 
 if __name__ == "__main__":
